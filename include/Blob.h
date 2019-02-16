28651f2bf8d786c2827f86fc21a539f074e80dad
Shreyas P V <shreyaspv25@gmail.com>
2019-02-16 12:48:42 +0530

    Updated headers to match new libraries

--- include/Blob.h
+++ include/Blob.h
@@ -8,6 +8,8 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/ml.hpp"
 #include "opencv2/objdetect.hpp"
+#include "opencv2/imgproc/types_c.h"
+#include "opencv2/videoio/legacy/constants_c.h"
 
 #include <iostream>
 
